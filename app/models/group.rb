b2a6fc41a5db662edbf08fcf5b7ff408f7428bf8
Ian Campbell <ianwcampbell.contact@gmail.com>
2015-10-01 17:54:37 -0400

    fixed db associations

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,7 @@ class Group < ActiveRecord::Base
   validates_presence_of :user_id, :lunch_id
   validates_uniqueness_of :user_id, scope: :lunch_id
 
-  belongs_to :lunches
-  belongs_to :users
-  
+  belongs_to :lunch
+  belongs_to :user
+
 end
